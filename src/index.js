1991e7eb272d49fc1d4694bd7c5fc24b218294be
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-10-03 17:32:57 -0500

    emits events (on, once, off) and broadcast events

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const io = new Server(httpServer);
 
 app.use(express.static(path.join(__dirname, "views")));
 
+const socketsOnLine = [];
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/view/index.html");
 });
@@ -24,6 +26,55 @@ io.on("connection", (socket) => {
   //       "Hemos pasado de HTTP Long-Polling a " + socket.conn.transport.name
   //     );
   //   });
+
+  socketsOnLine.push(socket.id);
+
+  // Emisión básica de eventos
+
+  socket.emit("bienvenido", "Ahora estás conectado 😎");
+
+  socket.on("cliente", (data) => {
+    console.log(data);
+    io.emit("todos", data);
+  });
+
+  // Emisión a todos los clientes
+  io.emit("todos", socket.id + " se ha conectado");
+
+  // Emisión a uno solo
+  socket.on("saludar_last", (data) => {
+    const lastSocket = socketsOnLine.at(-1);
+    io.to(lastSocket).emit("saludo", data);
+  });
+
+  // on -> se emite varias veces y se escucha todas las veces
+  //   socket.emit("on", "Hola a On");
+  //   socket.emit("on", "Hola a On");
+
+  // once -> se emite varias veces, pero se escucha una sola vez
+  //   socket.emit("once", "Hola a Once");
+  //   socket.emit("once", "Hola a Once");
+
+  // off -> se emite el evento, pero se deja de escuchar en el cliente
+  //   socket.emit("apagar", "Hola desde Off");
+
+  //   setTimeout(() => {
+  //     socket.emit("apagar", "Hola desde Off");
+  //   }, 3000);
+
+  socket.on("circle_position", (position) => {
+    // io.emit -> emite el evento a todos los clientes, incluso al que lo llamó originalmente
+    // io.emit("circle_move", position);
+
+    // socket.broadcast.emit -> emite el evento a todos los clientes, menos al cliente que lo llamo
+    socket.broadcast.emit("circle_move", position);
+  });
 });
+
+/*
+    On → Se usa para detectar (o escuchar) un evento varias veces.
+    Once → Se usa para detectar (o escuchar) un evento una sola vez. Sin importar si el evento se emite varias veces.
+    Off → Se usa para dejar de escuchar un evento, sin importar que este se siga emitiendo. 
+*/
 
 httpServer.listen(3000);
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,27 +1,116 @@
 const socket = io();
 
-function checkSocketStatus() {
-  console.log("Estado del socket:", socket.connected);
-}
+// function checkSocketStatus() {
+//   console.log("Estado del socket:", socket.connected);
+// }
 
-socket.on("connect", () => {
-  console.log("El socket se ha conectado:", socket.id);
-  checkSocketStatus();
+// socket.on("connect", () => {
+//   console.log("El socket se ha conectado:", socket.id);
+//   checkSocketStatus();
+// });
+
+// socket.on("disconnect", () => {
+//   console.log("El socket se ha desconectado:", socket.id);
+//   checkSocketStatus();
+// });
+
+// socket.on("connect_error", () => {
+//   console.log("No se pudo conectar.");
+// });
+
+// socket.io.on("reconnect_attempt", () => {
+//   console.log("Intentando reconectar...");
+// });
+
+// socket.io.on("reconnect", () => {
+//   console.log("¡Reconexión exitosa!");
+// });
+
+const text = document.getElementById("text");
+const btn = document.getElementById("enviar");
+const btnSaludar = document.getElementById("saludar");
+const mensaje = document.getElementById("mensaje");
+const mensajes = document.getElementById("mensajes");
+
+socket.on("bienvenido", (data) => {
+  console.log(data);
+  text.innerText = data;
 });
 
-socket.on("disconnect", () => {
-  console.log("El socket se ha desconectado:", socket.id);
-  checkSocketStatus();
+socket.on("todos", (data) => {
+  mensajes.innerHTML += data + "<br>";
 });
 
-socket.on("connect_error", () => {
-  console.log("No se pudo conectar.");
+socket.on("saludo", (data) => {
+  mensajes.innerHTML += data + "<br>";
 });
 
-socket.io.on("reconnect_attempt", () => {
-  console.log("Intentando reconectar...");
+btn.addEventListener("click", () => {
+  socket.emit("cliente", mensaje.value);
+  mensaje.value = "";
 });
 
-socket.io.on("reconnect", () => {
-  console.log("¡Reconexión exitosa!");
+btnSaludar.addEventListener("click", () => {
+  socket.emit("saludar_last", "Bienvenido al grupo");
+});
+
+/*
+    On → Se usa para detectar (o escuchar) un evento varias veces.
+    Once → Se usa para detectar (o escuchar) un evento una sola vez. Sin importar si el evento se emite varias veces.
+    Off → Se usa para dejar de escuchar un evento, sin importar que este se siga emitiendo. 
+*/
+
+// socket.on("on", () => {
+//   console.log("Se emite varias veces");
+// });
+
+// socket.once("once", () => {
+//   console.log("Se emite una sola vez");
+// });
+
+// const listener = () => {
+//   console.log("Se va a apagar el evento");
+// };
+
+// socket.on("apagar", listener);
+
+// setTimeout(() => {
+//   socket.off("apagar", listener);
+// }, 2000);
+
+const circle = document.getElementById("circle");
+
+const dragcircle = (position) => {
+  circle.style.top = position.top;
+  circle.style.left = position.left;
+};
+
+const drag = (e) => {
+  //   console.log(e);
+  //   const clientX = e.clientX;
+  //   const clientY = e.clientY;
+
+  const position = {
+    top: e.clientY + "px",
+    left: e.clientX + "px",
+  };
+
+  dragcircle(position);
+
+  socket.emit("circle_position", position);
+
+  //   circle.style.top = clientY + "px";
+  //   circle.style.left = clientX + "px";
+};
+
+document.addEventListener("mousedown", (e) => {
+  document.addEventListener("mousemove", drag);
+});
+
+document.addEventListener("mouseup", (e) => {
+  document.removeEventListener("mousemove", drag);
+});
+
+socket.on("circle_move", (position) => {
+  dragcircle(position);
 });
